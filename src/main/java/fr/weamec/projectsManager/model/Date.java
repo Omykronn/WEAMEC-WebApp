07e46c3ed30ed138d34404fc09432eeb11ce3eee
Omykronn <omykronn.player@gmail.com>
2024-10-30 18:55:20 +0100

    Création de la classe-modèle Date et d'un de ses tests

---
+++ src/main/java/fr/weamec/projectsManager/model/Date.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.weamec.projectsManager.model;
+
+/**
+ * Classe représentant une date
+ * @author simon
+ */
+public class Date {
+    private int jour;
+    private int mois;
+    private int annee;
+    
+    /**
+     * Constructeur de Date
+     * @author simon
+     * @param jour  Jour
+     * @param mois  Mois
+     * @param annee Annee
+     * @throws IllegalArgumentException Valeur de jour / mois illicite
+     */
+    public Date(int jour, int mois, int annee) throws IllegalArgumentException {
+        if (jour < 1 || jour > 31) {
+            throw new IllegalArgumentException("Valeur de jour illicite");
+        }
+        
+        if (mois < 1 || mois > 12) {
+            throw new IllegalArgumentException("Valeur de mois illicite");
+        }
+        
+        this.jour = jour;
+        this.mois = mois;
+        this.annee = annee;
+    }
+    
+    /**
+     * Conversion de la date en chaîne de caractères selon la norme ISO 8601 (compatible avec PostgreSQL)
+     * @author simon
+     * @return Date sous forme de chaîne de caractères
+     */
+    public String toString() {
+        return String.format("%04d", annee) + "-" + String.format("%02d", mois) + "-" + String.format("%02d", jour);
+    }
+}
--- src/test/java/fr/weamec/projectsManager/ProjectsManagerApplicationTests.java
+++ src/test/java/fr/weamec/projectsManager/ProjectsManagerApplicationTests.java
@@ -1,13 +1,26 @@
 package fr.weamec.projectsManager;
 
+import fr.weamec.projectsManager.model.Date;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjectsManagerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
+    
+    /**
+     * Test si la conversion de date fonctionne
+     * @author simon
+     * @throws Exception Test echoue
+     */
+    @Test
+    void dateWellConverts() throws Exception {
+        Date date = new Date(1, 6, 55);
+        
+        if (!"0055-06-01".equals(date.toString())) {
+            throw new Exception();
+        }
+    }
 }
