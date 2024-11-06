c75a1e57fda54d2d717e647ccbeadb25e42030b9
Omykronn <omykronn.player@gmail.com>
2024-11-06 07:54:13 +0100

    Page Home (non paramétrable)

--- src/main/java/fr/weamec/projectsManager/controller/ProjetController.java
+++ src/main/java/fr/weamec/projectsManager/controller/ProjetController.java
@@ -6,11 +6,13 @@ package fr.weamec.projectsManager.controller;
 
 import fr.weamec.projectsManager.model.*;
 import fr.weamec.projectsManager.service.*;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 /**
@@ -20,10 +22,35 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ProjetController {
     @Autowired
+    ProjetService projService;
+    
+    @Autowired
     CoordinateurScientifiqueService csService;
     
     @Autowired
     StructureRattachementService srService;
+    
+    @GetMapping("/home")
+    public String home(Model model) {
+        int id = 3;
+        
+        Optional<CoordinateurScientifique> csOpt = csService.getCoordinateurScientifique(id);
+        String template;
+        
+        if (csOpt.isPresent()) {
+            CoordinateurScientifique cs = csOpt.get();
+            
+            model.addAttribute("personne", cs);
+            model.addAttribute("projets", projService.getProjetsByCoordinateurScientifiqueId(cs.getId()));
+            
+            template = "home";
+        }
+        else {
+            template = "notFound";
+        }
+        
+        return template;
+    }
     
     @GetMapping("/newStructureRattachement")
     public String createAccount() {
