96a1b58a2618ff6f37da1cea05ec560ec58da6ff
Omykronn <omykronn.player@gmail.com>
2024-11-12 15:38:21 +0100

    Re-initialisation du frontend

--- src/main/java/fr/weamec/projectsManager/controller/ProjetController.java
+++ src/main/java/fr/weamec/projectsManager/controller/ProjetController.java
@@ -4,17 +4,9 @@
  */
 package fr.weamec.projectsManager.controller;
 
-import fr.weamec.projectsManager.model.*;
 import fr.weamec.projectsManager.service.*;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
 /**
  * Controller pour Projet
  * @author simon
@@ -29,57 +21,4 @@ public class ProjetController {
     
     @Autowired
     StructureRattachementService srService;
-    
-    @GetMapping("/home/{id}")
-    public String home(Model model, @PathVariable int id) {        
-        Optional<CoordinateurScientifique> csOpt = csService.getCoordinateurScientifique(id);
-        String template;
-        
-        if (csOpt.isPresent()) {
-            CoordinateurScientifique cs = csOpt.get();
-            
-            model.addAttribute("personne", cs);
-            model.addAttribute("projets", projService.getProjetsByCoordinateurScientifiqueId(cs.getId()));
-            
-            template = "home";
-        }
-        else {
-            template = "notFound";
-        }
-        
-        return template;
-    }
-    
-    @GetMapping("/newStructureRattachement")
-    public String createAccount() {
-        return "formNewStructureRattachement";
-    }
-    
-    @PostMapping("/saveStructureRattachement")
-    public String saveStructureRattachement(@ModelAttribute StructureRattachement structureRattachement, Model model) {
-        // Sauvegarde de la nouvelle structure
-        structureRattachement.setDir("zrbzoubev");
-        StructureRattachement sc = srService.save(structureRattachement);
-        
-        return "done";	
-    }
-    
-    @GetMapping("/newCoordinateurScientifique")
-    public String newCoordinateurScientitifque() {
-        return "formNewCoordinateurScientifique";
-    }
-    
-    @PostMapping("/saveCoordinateurScientifique")
-    public String saveCoordinateurScientifique(@ModelAttribute CoordinateurScientifique coordinateurScientifique,
-                                               @ModelAttribute StructureRattachement structureRattachement) {
-        // Sauvegarde de la structure de rattachement
-        structureRattachement.setDir("helloworld");
-        structureRattachement = srService.save(structureRattachement);
-        
-        // Sauvegarde du coordinateurScientifique
-        coordinateurScientifique.setStructureRattachement(structureRattachement);
-        coordinateurScientifique = csService.save(coordinateurScientifique);
-        
-        return "done";
-    }
 }
