3aa7b3ffa0fc1378710f9fe6844b035ac304ee69
Omykronn <omykronn.player@gmail.com>
2024-11-17 21:16:25 +0100

    viewProject & editProject controller functions

--- src/main/java/fr/weamec/projectsManager/controller/ProjetController.java
+++ src/main/java/fr/weamec/projectsManager/controller/ProjetController.java
@@ -4,9 +4,15 @@
  */
 package fr.weamec.projectsManager.controller;
 
+import fr.weamec.projectsManager.model.Projet;
 import fr.weamec.projectsManager.service.*;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 /**
  * Controller pour Projet
  * @author simon
@@ -14,11 +20,72 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ProjetController {
     @Autowired
-    ProjetService projService;
+    ProjetService projetService;    
     
     @Autowired
-    CoordinateurScientifiqueService csService;
+    CategorieService categorieService;
     
     @Autowired
-    StructureRattachementService srService;
+    TypeService typeService;
+    
+    @Autowired
+    PrioriteService prioriteService;
+    
+    @Autowired
+    ObjectifService objectifService;
+    
+    @Autowired 
+    DefiService defiService;
+    
+    @Autowired
+    ValeurService valeurService;
+    
+    @Autowired
+    ThemeService themeService;
+    
+    @Autowired
+    TechnologieService technologieService;
+    
+    @GetMapping("/viewProject/idProjet={id}")
+    public String viewProject(@PathVariable("id") int id, Model model) {
+        String pageName;
+        Optional<Projet> projet = projetService.getProjet(id);
+        
+        if (projet.isPresent()) {
+            pageName = "viewProjet";
+            model.addAttribute("projet", projet.get());
+        }
+        else {
+            pageName = "unknownProject";
+        }
+        
+        return pageName;
+    }
+    
+    @GetMapping("/editProject/idProjet={id}")
+    public String editProject(@PathVariable("id") int id, Model model) {
+        String pageName;
+        Optional<Projet> projet = projetService.getProjet(id);
+        
+        if (projet.isPresent()) {
+            pageName = "formProjet";
+            model.addAttribute("projet", projet.get());
+            
+            model.addAttribute("allPriorites", prioriteService.getPriorites());
+            model.addAttribute("allObjectifs", objectifService.getObjectifs());
+            model.addAttribute("allDefis", defiService.getDefis());
+            
+            model.addAttribute("allValeurs", valeurService.getValeurs());
+            model.addAttribute("allThemes", themeService.getThemes());
+            model.addAttribute("allTechnologies", technologieService.getTechnologies());
+            
+            model.addAttribute("allCategories", categorieService.getCategories());
+            model.addAttribute("allTypes", typeService.getTypes());
+        }
+        else {
+            pageName = "unknownProject";
+        }
+        
+        return pageName;
+    }
 }
