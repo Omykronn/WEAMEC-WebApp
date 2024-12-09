5a3545602e2cf986bcc1b369b6ad3ebe7042fb3d
Omykronn <omykronn.player@gmail.com>
2024-12-09 19:57:52 +0100

    Test : import de Expert avec un fichier JSON

---
+++ src/test/java/fr/weamec/projectsManager/model/ExpertTest.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package fr.weamec.projectsManager.model;
+
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author simon
+ */
+public class ExpertTest {
+    
+    public ExpertTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of import from JSON.
+     */
+    @Test
+    public void testImportFromJson() throws IOException, ParseException {
+        System.out.println("importFromJSON : Expert");
+        
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse(new FileReader("src/test/resources/json/test_Expert.json"));
+        Expert instance = new Expert(json);
+        
+        assertEquals(instance.getNom(), "DOE");
+        assertEquals(instance.getPrenom(), "John");
+        assertEquals(instance.getEntiteRattachement(), "École Centrale de Nantes");
+        assertEquals(instance.getLaboratoireRattachement(), "INFOSI");
+        assertEquals(instance.getSpecialite(), "JAVA Spring Boot");
+        assertEquals(instance.getMail(), "john.doe@eleves.ec-nantes.fr");
+        assertEquals(instance.getTelephone(), "+33 7 89 01 23 45");
+    }   
+}
