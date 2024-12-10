49f8c4a38f11aff370aaad8f5f43dce9051e0c26
Omykronn <omykronn.player@gmail.com>
2024-12-10 15:31:39 +0100

    Recherche du dossier des avis d'un projet

--- src/main/java/fr/weamec/projectsManager/service/file/FileSystemService.java
+++ src/main/java/fr/weamec/projectsManager/service/file/FileSystemService.java
@@ -94,4 +94,14 @@ public class FileSystemService {
         
         return findFile(dir, extensions);
     }
+    
+    /**
+     * Retourne l'objet File associé au dossier des avis d'un projet
+     * @param idProjet  Identifiant d'un projet
+     * @return          Objet file du dossier
+     * @throws IOException 
+     */
+    public File getDossierAvis(int idProjet) throws IOException {        
+        return new File(storageDirectory + "/project" + String.format("%08d", idProjet) + "/avisMotives");
+    }
 }
