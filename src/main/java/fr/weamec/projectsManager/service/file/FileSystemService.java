f1981895bc5e1479bb93be8907fcdab04c1143af
Omykronn <omykronn.player@gmail.com>
2024-12-10 15:12:51 +0100

    Recherche du fichier stockant le visuel d'un projet

---
+++ src/main/java/fr/weamec/projectsManager/service/file/FileSystemService.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.weamec.projectsManager.service.file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service pour l'accès aux fichiers sur le serveur
+ * @author simon
+ */
+@Service
+public class FileSystemService {
+    private final String storageDirectory = "/home/simon/Documents/INFOSI/00_PAPPL/projectStorage";
+    
+    /**
+     * Test une liste d'extensions jusqu'à trouver un fichier correspondant
+     * @param dir           Chemin d'accès au fichier AVEC POINT ET SANS EXTENSION
+     * @param extensions    Liste d'extensions à chercher
+     * @return              Fichier
+     * @throws FileNotFoundException 
+     */
+    private File findFile(String dir, String[] extensions) throws FileNotFoundException {
+        File file = null;
+        File tempFile;
+        
+        for (String ext: extensions) {
+            tempFile = new File(dir + ext);
+            
+            if (tempFile.exists()) {
+                file = tempFile;
+            }
+        }
+        
+        if (file == null) {
+            throw new FileNotFoundException();
+        }
+        
+        return file;
+    }
+    
+    public File getVisuel(int idProjet) throws IOException {
+        String dir = storageDirectory + "/project" + String.format("%08d", idProjet) + "/visuel.";
+        String[] extensions = {"png", "jpg", "jpeg"};
+        
+        return findFile(dir, extensions);
+    }
+}
